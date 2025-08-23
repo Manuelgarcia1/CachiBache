01e2566fae5106b80cb631d7234c9a507fca8e14
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2025-08-23 19:01:09 -0300

    Mejorar AppLogo con estilos de Tamagui

--- src/features/auth/components/AppLogo.tsx
+++ src/features/auth/components/AppLogo.tsx
@@ -1,15 +1,16 @@
 import { Stack } from "@tamagui/core";
-import { Image } from "react-native";
+import { Image } from "expo-image";
 
 export function AppLogo() {
   return (
     <Stack space="$4" alignItems="center" marginBottom="$8">
-      <Image
-        source={require("../../../../assets/images/icon.png")}
-        width={290}
-        height={290}
-        resizeMode="cover"
-      />
+      <Stack width={290} height={290}>
+        <Image
+          source={require("../../../../assets/images/icon.png")}
+          style={{ flex: 1 }}
+          contentFit="cover"
+        />
+      </Stack>
     </Stack>
   );
 }
